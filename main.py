758319f16ca454863dd1f53da62034d3af56bf88
cb54 <32172814+carlobee@users.noreply.github.com>
2020-11-16 21:29:09 +0000

    Early program design

---
+++ main.py
@@ -0,0 +1,29 @@
+"""
+A program that implements a basic feedforward multilayer Artificial Neural Network
+
+Written By Carl Bettosi and Mamta Sofat
+16/11/202
+
+"""
+
+# take inputs
+
+
+
+# add bias
+
+
+
+# assign random weights to hidden and output layer
+
+
+
+# define activation functions
+
+
+
+# run training code (do this last - PSO)
+
+
+
+# Find error in predication
